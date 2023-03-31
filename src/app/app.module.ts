a8fb6f5622ca8c5e57e2afd82046315f625c5737
Parfs <vincejerome.parafina@gmail.com>
2023-03-31 16:15:00 +0800

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,31 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import {ProductService} from './product.service';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent},
+  { path: 'product/:id', component: ProductPageComponent},
+  { path: '', redirectTo: 'products', pathMatch: 'full'}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDescriptionComponent,
+    ProductTracklistingComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
